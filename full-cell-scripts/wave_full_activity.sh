b73c4f282932c574c2a6e3df6458a6de4c4c3402
Stephan Grein <stephan@syntaktischer-zucker.de>
2020-08-20 10:16:09 +0200

    Adding full-cell-scripts and reorganizing repo.

---
+++ full-cell-scripts/wave_full_activity.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#PBS -l walltime=06:00:00
+#PBS -V
+#PBS -m bea
+#PBS -N wave3d_revised_multi_measure_zones_FULL_ACTIVITY
+#PBS -q normal
+#PBS -l nodes=1:ppn=1
+cd $PBS_O_WORKDIR
+SCRIPT=wave3d_revised_multi_measure_zones_FULL_ACTIVITY.lua
+GRID=/home/tug41634/single_branch2_w_branch_meas_new.ugx
+SOLVER=GMG
+mpirun -np 1  ugshell -ex "$SCRIPT" -grid "$GRID" -dt 1e-6 -endTime 100.00 -solver "$SOLVER" -tol 0.02 -ryrDensity 10 -setting ryr -vtk -pstep 0.0 -outName /home/tug41634 -vtkFolder vtk7 -suffix vtk7/meas/ -measSuffix meas -vtk
